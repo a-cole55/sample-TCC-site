e426e784aef685ed601c08b5e613838083f2bdd9
a-cole55 <arniqua_c@yahoo.com>
2022-05-01 21:54:07 -0500

    updated contact form

--- TCC.js
+++ TCC.js
@@ -1,3 +1,23 @@
+document.querySelector('#menuBG').addEventListener('mouseover', openNav);
+document.querySelector("#main").addEventListener('mouseover', closeNav);
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.6)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+}
+
+
+
 //Read More and Read Less Functionality
 function myFunction() {
     const dots = document.getElementById("dots");
